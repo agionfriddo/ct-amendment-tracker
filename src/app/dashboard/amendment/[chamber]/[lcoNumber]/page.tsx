80d2fc0dffb30cde6d4cf33f724e8b6f682bc5c2
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2025-03-11 15:17:25 -0400

    Add a compare to bill button

--- src/app/dashboard/amendment/[chamber]/[lcoNumber]/page.tsx
+++ src/app/dashboard/amendment/[chamber]/[lcoNumber]/page.tsx
@@ -287,6 +287,12 @@ export default function AmendmentDetailPage() {
             >
               Go Back
             </button>
+            <Link
+              href={`/dashboard/bill/${amendment.billNumber}/compare-amendment-to-bill?lco=${amendment.lcoNumber}`}
+              className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-2"
+            >
+              Compare to Bill
+            </Link>
             <a
               href={amendment.lcoLink}
               target="_blank"
