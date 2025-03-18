3c2fe3a703486bc2c3485fa20090879bdbbd8584
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2025-03-17 22:29:05 -0400

    Add credentials

--- src/app/api/utils/dynamoClient.ts
+++ src/app/api/utils/dynamoClient.ts
@@ -1,5 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
 
 export const client = new DynamoDBClient({
   region: process.env.AWS_REGION || "us-east-1",
+  credentials: defaultProvider(),
 });
